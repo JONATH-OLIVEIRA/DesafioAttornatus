3c686f2294c3293118d995fbc4bfba0cf60793dc
Jonath Oliveira <akiles22a@gmail.com>
2023-01-09 16:17:00 -0300

    criacao do projeto backend

---
+++ apiclient/src/main/resources/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	spring.profiles.active=test
+	spring.jpa.defer-datasource-initialization=true
+	spring.jpa.open-in-view=false
+	
+	server.port= 8090
+}
+
